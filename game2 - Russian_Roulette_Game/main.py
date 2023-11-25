38f99de3762ad089610ebe1aecde0d0378b8ba01
_kewwi ng_ <102494428+cuddles47@users.noreply.github.com>
2023-11-25 14:50:57 +0700

    Update main.py

--- game2 - Russian_Roulette_Game/main.py
+++ game2 - Russian_Roulette_Game/main.py
@@ -2,7 +2,7 @@ import random
 import os
 
 number = random.randint(0,7)                  #1-6 tradictional russian roulette :))
-guess = int(input("Guess a number between 1 and 10: "))
+guess = int(input("Guess a number between 1 and 6: "))
 guess = int(guess)
 
 if guess == number:
